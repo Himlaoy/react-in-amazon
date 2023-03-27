d051953397328e004749f3ffedeaa2b12615e6cc
himaloyash <himaloyashis98@gmail.com>
2023-03-27 16:04:09 +0600

    add to card btn added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import './Header.css'
+import logo from '../../images/Logo.svg'
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is header</h2>
-        </div>
+        <nav className='Header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/order">Order</a>
+                <a href="/inventory">Inventory</a>
+                <a href="Login">LogIn</a>
+            </div>
+
+        </nav>
     );
 };
 
